b17132156be4580cda6e6845a2b5a558adb01f95
arifinmn <nurarifinmuhammad@gmail.com>
2021-03-23 00:00:41 +0700

    handle redirect from dashboard to page login

---
+++ src/configs/constants/roles/roleConstant.js
@@ -0,0 +1,2 @@
+export const STAFF = 'STAFF'
+export const ADMIN = 'ADMIN'
--- src/configs/routes/index.js
+++ src/configs/routes/index.js
@@ -1,17 +1,24 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import React, { useState } from "react";
+import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom";
 import Home from "../../pages";
 import Dashboard from "../../pages/Dashboard";
 import Login from "../../pages/Login";
 import Register from "../../pages/Register";
+import { ADMIN, STAFF } from "../constants/roles/roleConstant";
 
 const Routes = () => {
+  const [token, setToken] = useState(localStorage.getItem("token"))
+  const [role, setRole] = useState(localStorage.getItem("role"))
+
   return (
     <Router>
       <Switch>
         <Route path="/login" component={Login} exact />
         <Route path="/register" component={Register} exact />
-        <Route path="/dashboard" component={Dashboard} />
+        <Route path="/dashboard" render={() => (
+          (role == ADMIN || role == STAFF) && token ?
+            <Dashboard /> : <Redirect to="/login" />
+        )} />
         <Route path="/" component={Home} exact />
       </Switch>
     </Router>
