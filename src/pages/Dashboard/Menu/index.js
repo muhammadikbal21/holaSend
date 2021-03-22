e30f0237709bc794669c242807aafa57ac1496b7
arifinmn <nurarifinmuhammad@gmail.com>
2021-03-22 23:21:54 +0700

    add logout menu with sweatallert

--- src/pages/Dashboard/Menu/index.js
+++ src/pages/Dashboard/Menu/index.js
@@ -1,8 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+import swal from "sweetalert"
 
 const Menu = () => {
+  const [role, setRole] = useState(localStorage.getItem("role"))
+
+  const handleLogout = (e) => {
+    swal({
+      title: "Logout?",
+      text: "Are you sure want to logout now?",
+      icon: "warning",
+      buttons: true,
+      dangerMode: true,
+    })
+      .then((logout) => {
+        if (logout) {
+          localStorage.removeItem("token")
+          localStorage.removeItem("role")
+          window.location.href = "/"
+        }
+      });
+  }
+
   return (
-    <aside className="main-sidebar sidebar-dark-primary elevation-4" style={{backgroundColor: '#152C5B'}}>
+    <aside className="main-sidebar sidebar-dark-primary elevation-4" style={{ backgroundColor: '#152C5B' }}>
       {/* Brand Logo */}
       <div className="brand-link">
         <img
@@ -10,7 +30,7 @@ const Menu = () => {
           alt="hola!"
           className="brand-image img-circle elevation-3"
         />
-        <span style={{color: '#536DFE', fontWeight: 'bold'}}>&nbsp; hola<span className="brand-text font-weight-light" style={{color: 'white', fontWeight: 'bold'}}>Send!</span></span>
+        <span style={{ color: '#536DFE', fontWeight: 'bold' }}>&nbsp; hola<span className="brand-text font-weight-light" style={{ color: 'white', fontWeight: 'bold' }}>Send!</span></span>
       </div>
       {/* Sidebar */}
       <div className="sidebar">
@@ -18,7 +38,7 @@ const Menu = () => {
         <div className="user-panel mt-3 pb-3 mb-3 d-flex">
           <div className="info">
             <a href="#" className="d-block">
-            &nbsp; ADMIN
+              &nbsp; ADMIN
             </a>
           </div>
         </div>
@@ -32,8 +52,8 @@ const Menu = () => {
           >
             {/* Add icons to the links using the .nav-icon class
              with font-awesome or any other icon font library */}
-            <li className="nav-item has-treeview" style={{marginBottom: '3px'}}>
-              <a href="#" className="nav-link active" style={{backgroundColor: '#536DFE'}}>
+            <li className="nav-item has-treeview" style={{ marginBottom: '3px' }}>
+              <a href="#" className="nav-link active" style={{ backgroundColor: '#536DFE' }}>
                 <i className="nav-icon fas fa-edit" />
                 <p>
                   Tasks
@@ -54,8 +74,8 @@ const Menu = () => {
                 </li>
               </ul>
             </li>
-            <li className="nav-item has-treeview" style={{marginBottom: '3px'}}>
-              <a href="#" className="nav-link active" style={{backgroundColor: '#536DFE'}}>
+            <li className="nav-item has-treeview" style={{ marginBottom: '3px' }}>
+              <a href="#" className="nav-link active" style={{ backgroundColor: '#536DFE' }}>
                 <i className="nav-icon fas fa-map-marked-alt" />
                 <p>
                   Destinations
@@ -76,84 +96,96 @@ const Menu = () => {
                 </li>
               </ul>
             </li>
-            <li className="nav-item has-treeview" style={{marginBottom: '3px'}}>
-              <a href="#" className="nav-link active" style={{backgroundColor: '#536DFE'}}>
-                <i className="nav-icon fas fa-user" />
+            {
+              role == "ADMIN" ?
+                <>
+                  <li className="nav-item has-treeview" style={{ marginBottom: '3px' }}>
+                    <a href="#" className="nav-link active" style={{ backgroundColor: '#536DFE' }}>
+                      <i className="nav-icon fas fa-user" />
+                      <p>
+                        Users
+                </p>
+                    </a>
+                    <ul className="nav nav-treeview">
+                      <li className="nav-item">
+                        <a href="/dashboard/user-managements" className="nav-link">
+                          <i className="fas fa-users-cog nav-icon" />
+                          <p>User Managements</p>
+                        </a>
+                      </li>
+                    </ul>
+                  </li>
+                  <li className="nav-item has-treeview" style={{ marginBottom: '3px' }}>
+                    <a href="#" className="nav-link active" style={{ backgroundColor: '#536DFE' }}>
+                      <i className="nav-icon fas fa-copy" />
+                      <p>
+                        Reports
+                </p>
+                    </a>
+                    <ul className="nav nav-treeview">
+                      <li className="nav-item">
+                        <a href="pages/layout/top-nav.html" className="nav-link">
+                          <i className="far fa-circle nav-icon" />
+                          <p>Top Navigation</p>
+                        </a>
+                      </li>
+                      <li className="nav-item">
+                        <a
+                          href="pages/layout/top-nav-sidebar.html"
+                          className="nav-link"
+                        >
+                          <i className="far fa-circle nav-icon" />
+                          <p>Top Navigation + Sidebar</p>
+                        </a>
+                      </li>
+                      <li className="nav-item">
+                        <a href="pages/layout/boxed.html" className="nav-link">
+                          <i className="far fa-circle nav-icon" />
+                          <p>Boxed</p>
+                        </a>
+                      </li>
+                      <li className="nav-item">
+                        <a
+                          href="pages/layout/fixed-sidebar.html"
+                          className="nav-link"
+                        >
+                          <i className="far fa-circle nav-icon" />
+                          <p>Fixed Sidebar</p>
+                        </a>
+                      </li>
+                      <li className="nav-item">
+                        <a href="pages/layout/fixed-topnav.html" className="nav-link">
+                          <i className="far fa-circle nav-icon" />
+                          <p>Fixed Navbar</p>
+                        </a>
+                      </li>
+                      <li className="nav-item">
+                        <a href="pages/layout/fixed-footer.html" className="nav-link">
+                          <i className="far fa-circle nav-icon" />
+                          <p>Fixed Footer</p>
+                        </a>
+                      </li>
+                      <li className="nav-item">
+                        <a
+                          href="pages/layout/collapsed-sidebar.html"
+                          className="nav-link"
+                        >
+                          <i className="far fa-circle nav-icon" />
+                          <p>Collapsed Sidebar</p>
+                        </a>
+                      </li>
+                    </ul>
+                  </li>
+                </> : <></>
+            }
+            <li className="nav-item has-treeview" style={{ marginBottom: '3px' }} onClick={handleLogout}>
+              <a href="#" className="nav-link active" style={{ backgroundColor: '#536DFE' }}>
+                <i className="nav-icon fa fa-exclamation-triangle" />
                 <p>
-                  Users
+                  Logout
                 </p>
               </a>
-              <ul className="nav nav-treeview">
-                <li className="nav-item">
-                  <a href="/dashboard/user-managements" className="nav-link">
-                    <i className="fas fa-users-cog nav-icon" />
-                    <p>User Managements</p>
-                  </a>
-                </li>
-              </ul>
             </li>
-            <li className="nav-item has-treeview" style={{marginBottom: '3px'}}>
-              <a href="#" className="nav-link active" style={{backgroundColor: '#536DFE'}}>
-                <i className="nav-icon fas fa-copy" />
-                <p>
-                  Reports
-                </p>
-              </a>
-              <ul className="nav nav-treeview">
-                <li className="nav-item">
-                  <a href="pages/layout/top-nav.html" className="nav-link">
-                    <i className="far fa-circle nav-icon" />
-                    <p>Top Navigation</p>
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a
-                    href="pages/layout/top-nav-sidebar.html"
-                    className="nav-link"
-                  >
-                    <i className="far fa-circle nav-icon" />
-                    <p>Top Navigation + Sidebar</p>
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a href="pages/layout/boxed.html" className="nav-link">
-                    <i className="far fa-circle nav-icon" />
-                    <p>Boxed</p>
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a
-                    href="pages/layout/fixed-sidebar.html"
-                    className="nav-link"
-                  >
-                    <i className="far fa-circle nav-icon" />
-                    <p>Fixed Sidebar</p>
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a href="pages/layout/fixed-topnav.html" className="nav-link">
-                    <i className="far fa-circle nav-icon" />
-                    <p>Fixed Navbar</p>
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a href="pages/layout/fixed-footer.html" className="nav-link">
-                    <i className="far fa-circle nav-icon" />
-                    <p>Fixed Footer</p>
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a
-                    href="pages/layout/collapsed-sidebar.html"
-                    className="nav-link"
-                  >
-                    <i className="far fa-circle nav-icon" />
-                    <p>Collapsed Sidebar</p>
-                  </a>
-                </li>
-              </ul>
-            </li>
-            
           </ul>
         </nav>
         {/* /.sidebar-menu */}
