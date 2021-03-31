61647ec5b1535636a6db50a5e01658a2fcab7d3c
Hariansyah <hariansyahhari2@gmail.com>
2021-03-31 21:09:14 +0700

    Add User Info

--- src/pages/Dashboard/Content/TasksList/index.js
+++ src/pages/Dashboard/Content/TasksList/index.js
@@ -190,19 +190,18 @@ const TasksList = (props) => {
                                     <table className="table text-nowrap table-bordered table-head-fixed">
                                         <thead>
                                             <tr>
-                                                <th>Created Date</th>
                                                 <th>Destination</th>
                                                 <th>Status</th>
                                                 <th>Priority</th>
                                                 <th>Notes</th>
                                                 <th>Requested By</th>
+                                                <th>Courier</th>
                                                 <th>Action</th>
                                             </tr>
                                         </thead>
                                         <tbody>
                                             {tasks.map((e) => (
                                                 <tr>
-                                                    <td>{e.createDate.substring(0, 19).replace("T", " ")}</td>
                                                     <td>
                                                         {e.destination.name}
                                                     </td>
@@ -210,10 +209,64 @@ const TasksList = (props) => {
                                                     <td>{e.priority}</td>
                                                     <td>{e.notes}</td>
                                                     <td>
+                                                        <ModalView
+                                                            className="fas fa-eye btn-primary"
+                                                            title="User Info"
+                                                            p1="Username"
+                                                            c1={e.requestBy.username}
+                                                            p2="Email"
+                                                            c2={e.requestBy.email}
+                                                            p3="Role"
+                                                            c3={e.requestBy.role}
+                                                            p4="First Name"
+                                                            c4={e.requestBy.userDetails.firstName}
+                                                            p5="Last Name"
+                                                            c5={e.requestBy.userDetails.lastName}
+                                                            p6="Indentity Category"
+                                                            c6={e.requestBy.userDetails.identityCategory}
+                                                            p7="Identification Number"
+                                                            c7={e.requestBy.userDetails.identificationNumber}
+                                                            p8="Contact Number"
+                                                            c8={e.requestBy.userDetails.contactNumber}
+                                                        />
+                                                        <span
+                                                            style={{
+                                                                margin: "3px",
+                                                            }}
+                                                        />
                                                         {e.requestBy
                                                             ? e.requestBy
                                                                   .username
                                                             : ""}
+                                                    </td>
+                                                    <td>
+                                                        {e.courier ?
+                                                            <ModalView
+                                                                className="fas fa-eye btn-primary"
+                                                                title="User Info"
+                                                                p1="Username"
+                                                                c1={e.courier.username}
+                                                                p2="Email"
+                                                                c2={e.courier.email}
+                                                                p3="Role"
+                                                                c3={e.courier.role}
+                                                                p4="First Name"
+                                                                c4={e.courier.userDetails.firstName}
+                                                                p5="Last Name"
+                                                                c5={e.courier.userDetails.lastName}
+                                                                p6="Indentity Category"
+                                                                c6={e.courier.userDetails.identityCategory}
+                                                                p7="Identification Number"
+                                                                c7={e.courier.userDetails.identificationNumber}
+                                                                p8="Contact Number"
+                                                                c8={e.courier.userDetails.contactNumber}
+                                                            /> : null}
+                                                        <span
+                                                            style={{
+                                                                margin: "3px",
+                                                            }}
+                                                        />
+                                                        {e.courier ? e.courier.username : ""}
                                                     </td>
                                                     <td>
                                                         <ModalView
